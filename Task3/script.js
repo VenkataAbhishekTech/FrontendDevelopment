9efe11749778b24dcfba0295e569b77e0a7dae7d
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-11-20 15:34:13 +0530

    Removed all files except README.md

--- Task3/script.js
+++
@@ -1,7 +0,0 @@
-function checkGoal() {
-    let steps = document.getElementById("steps").value;
-
-    let message = (steps >= 10000) ? "Goal Achieved 🎉" : "Keep Going 👣";
-
-    document.getElementById("result").innerText = message;
-}
