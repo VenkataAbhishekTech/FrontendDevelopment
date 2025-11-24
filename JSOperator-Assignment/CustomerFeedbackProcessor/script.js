34eae1e1352ae5a6865e41cedb3f7376136f1635
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-11-24 12:34:51 +0530

    Removed all files except README.md

--- JSOperator-Assignment/CustomerFeedbackProcessor/script.js
+++
@@ -1,7 +0,0 @@
-function processFeedback() {
-  let text = document.getElementById("feedback").value;
-  let words = text.trim().split(" ").length;
-  let negative = text.toLowerCase().includes("bad") || text.toLowerCase().includes("poor");
-  document.getElementById("words").innerText = "Word Count: " + words;
-  document.getElementById("result").innerText = negative ? "Needs Improvement" : "Positive Feedback";
-}
